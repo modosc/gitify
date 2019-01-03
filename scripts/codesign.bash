a8b64104a2cb4a76c627fcf7d819364789ad0796
jonathan schatz <jon@divisionbyzero.com>
2019-01-03 11:27:38 -0800

    update build/codesign

--- scripts/codesign.bash
+++ scripts/codesign.bash
@@ -1,1 +1,1 @@
-codesign --verbose --deep --force --sign "Developer ID Application: Emmanouil KONSTANTINIDIS" Gitify-darwin-x64/Gitify.app
+codesign --verbose --deep --force --sign "Developer ID Application: Jonathan Schatz" Gitify-darwin-x64/Gitify.app
